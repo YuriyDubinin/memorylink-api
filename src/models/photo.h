84b71a4657754d3da35080b15b4f9a1cdf511aa4
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-12-23 16:51:14 +0300

    upd

---
+++ src/models/photo.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <chrono>
+#include <cstdint>
+#include <optional>
+#include <string>
+
+struct Photo {
+    std::int64_t id;
+    std::int64_t family_id;
+
+    bool is_active = true;
+
+    double file_size_mb = 0.0;
+
+    std::string name;
+    std::string hash;
+    std::string mime_type;
+    std::string created_at;
+    std::string updated_at;
+
+    std::optional<std::string> description;
+    std::optional<double>      resolution_width_px;
+    std::optional<double>      resolution_height_px;
+};
---
+++ src/models/video.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <chrono>
+#include <cstdint>
+#include <optional>
+#include <string>
+
+struct Video {
+    std::int64_t id;
+    std::int64_t family_id;
+
+    double file_size_mb = 0.0;
+
+    bool is_active = true;
+
+    std::string name;
+    std::string hash;
+    std::string mime_type;
+    std::string created_at;
+    std::string updated_at;
+
+    std::optional<std::string> description;
+    std::optional<double>      resolution_width_px;
+    std::optional<double>      resolution_height_px;
+    std::optional<double>      duration_sec;
+    std::optional<double>      frame_rate;
+};
