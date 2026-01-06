6e390ea0fab3d7a7e5f6aeb1ce73ad6e8d58cad8
Yuriy Dubinin <yuradubinin100@gmail.com>
2026-01-06 20:05:00 +0300

    upd

---
+++ src/validators/upload_video_list_by_family_id.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+#include <httplib/httplib.h>
+
+#include <config/config_manager.h>
+#include <models/access_token_data.h>
+#include <models/api_response.h>
+#include <utils/access_token_crypto.h>
+#include <utils/extract_bearer_token.h>
+#include <utils/http_response.h>
+
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+namespace validate {
+    bool upload_video_list_by_family_id(const httplib::Request&    req,
+                                        const rapidjson::Document& body_json,
+                                        ApiResponse&               api_response);
+}
